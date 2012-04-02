d07942c9add6c371a5bb6d1236341416ef13f2d2
jmgodino <jmiguel.godino@catastro.minhap.es>
2012-04-02 13:06:27 +0200

    EVO: Los reintentos no se hacen a nivel de lógica de negocio sino del acceso al Stub RMI.

--- PdfServiceClient/src/main/java/es/gob/catastro/service/rmi/pdf/client/PDFClient.java
+++ PdfServiceClient/src/main/java/es/gob/catastro/service/rmi/pdf/client/PDFClient.java
@@ -4,28 +4,14 @@ import java.rmi.RemoteException;
 
 import es.gob.catastro.service.pdf.PDFService;
 import es.gob.catastro.service.pdf.util.PDFBuffer;
-
-import es.gob.catastro.service.rmi.client.RmiClientException;
 import es.gob.catastro.service.rmi.client.RmiOracleClient;
 
 public class PDFClient extends RmiOracleClient<PDFService> implements PDFService {
 
 	@Override
 	public PDFBuffer generarPDF(String xml, String trans) throws RemoteException {
-
-		for (int i = 0; i < getRetries(); i++) {
-			try {
-
-				PDFService service = getService("PdfService");
-				return service.generarPDF(xml, trans);
-
-			} catch (Exception e) {
-				debug("Error invocando al servicio", e);
-			}
-		}
-
-		throw new RmiClientException("No se ha podido invocar al servicio remoto");
-
+		PDFService service = getService("PdfService");
+		return service.generarPDF(xml, trans);
 	}
 
 }
