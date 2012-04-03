3ff60ac95135921f2758af43112c50b0cdb3c49f
jmgodino <jmiguel.godino@catastro.minhap.es>
2012-04-03 14:31:30 +0200

    EVO: Función estática en cliente para poder invocar desde PL/SQL. EVO: Se incluye en el manifest.mf de los jar, las librerías dependientes para asi no tener que configurar en el wrapper.conf todas las dependencias.

--- PdfServiceClient/src/main/java/es/gob/catastro/service/rmi/pdf/client/PDFClient.java
+++ PdfServiceClient/src/main/java/es/gob/catastro/service/rmi/pdf/client/PDFClient.java
@@ -3,15 +3,28 @@ package es.gob.catastro.service.rmi.pdf.client;
 import java.rmi.RemoteException;
 
 import es.gob.catastro.service.pdf.PDFService;
+import es.gob.catastro.service.pdf.PDFServiceException;
 import es.gob.catastro.service.pdf.util.PDFBuffer;
 import es.gob.catastro.service.rmi.client.RmiOracleClient;
 
 public class PDFClient extends RmiOracleClient<PDFService> implements PDFService {
 
 	@Override
-	public PDFBuffer generarPDF(String xml, String trans) throws RemoteException {
+	public PDFBuffer generarPDF(String xml, String trans) {
 		PDFService service = getService("PdfService");
-		return service.generarPDF(xml, trans);
+		try {
+			return service.generarPDF(xml, trans);
+		} catch (RemoteException e) {
+			throw new PDFServiceException(e);
+		}
+	}
+	
+	public static byte[] generarPdf(String xmlIn, String nombreTransformador){
+		return getInstance().generarPDF(xmlIn, nombreTransformador).getContent();
+	}
+	
+	private static PDFClient getInstance() {
+		return new PDFClient();
 	}
 
 }
