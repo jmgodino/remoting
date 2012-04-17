c52bf5fca3e8eade487d302b721cfa66047d3266
03877209F <jmiguel.godino@catastro.minhap.es>
2012-04-17 12:55:52 +0200

    EVO: Este proyecto no se necesita ya más.

--- RmiServer/run1099.bat
+++
@@ -1,2 +0,0 @@
-"C:\Archivos de programa\Java\jdk1.6.0_29\bin\java" -Dcom.sun.management.jmxremote -Dsun.rmi.transport.tcp.maxConnectionThreads=15 -Dserver.core.rmi.port=1099 -jar target\RmiServer-1.0-SNAPSHOT.jar
-pause 1000
